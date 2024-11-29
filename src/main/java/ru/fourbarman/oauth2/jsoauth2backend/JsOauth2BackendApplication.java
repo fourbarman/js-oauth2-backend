eee84debb876d7dbaae1e2125705babb636f4161
fourbarman <maks.java@yandex.ru>
2024-11-29 11:38:20 +0400

    init project

---
+++ src/main/java/ru/fourbarman/oauth2/jsoauth2backend/JsOauth2BackendApplication.java
@@ -0,0 +1,13 @@
+package ru.fourbarman.oauth2.jsoauth2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsOauth2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsOauth2BackendApplication.class, args);
+    }
+
+}
